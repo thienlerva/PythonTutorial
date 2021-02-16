70837f27bb1cf585a2ee9d95870225f87736277e
Thien Le <44382928+thienlerva@users.noreply.github.com>
2021-02-16 19:58:17 +0000

    Update main.py

--- MatplotLib/main.py
+++ MatplotLib/main.py
@@ -4,7 +4,7 @@ from sklearn.linear_model import LinearRegression
 
 data = pandas.read_csv('iphone_price.csv')
 # plt.scatter(data['version'], data['price'])
-# plt.show()
+plt.show()
 model = LinearRegression()
 model.fit(data[['version']], data[['price']])
 print(model.predict([[12]]))
